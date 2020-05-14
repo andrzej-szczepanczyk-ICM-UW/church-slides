2cdc42708110407c8630ff37e903a081a2b59a3e
andrzej1naukowiec <andrzej.tenor@gmail.com>
2020-05-14 17:52:40 +0200

    first commit

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
 // const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const redisServer = require('redis');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const redisServer = require("redis");
 const app = express();
 //password scalegrid - ScaleGrid007!
 
 //presentation part
 let PptxGenJS = require("pptxgenjs");
-let presentaation1 = new PptxGenJS();
-presentaation1.addSlide().addText('Niedziela 12 kwietnia', {x:1, y:2, w:'100%', color:'FFFFFF', fill:'000000', fontSize:24});
-presentaation1.addSlide().addText('Wejście1', {x:1, y:2, w:'100%', bkgd:'FFFFFF', color:'FFFFFF', fill:'000000', fontSize:24});
-presentaation1.writeFile('12-04-2020');
+let slides = new PptxGenJS();
+slides.layout({ bkgd: "36ABFF", color: "36ABFF" });
+slides.save();
 
+let SLIDE_MASTER_SETTINGS = {
+  title: "settings",
+  title: "Niedziela 2018-04-04",
+  bkgd: "36ABFF",
+  objects: [
+    {
+      placeholder: {
+        options: {
+          name: "name",
+          type: "body",
+        },
+      },
+    },
+  ],
+};
+
+text1 = "Duchu przyjdź";
+text2 = "Boże westchnij o nas";
+options = {
+  w: "100%",
+  h: "80%",
+  fill: "009920",
+  bkgd: "F0000",
+  color: "FFFFFF",
+  fontSize: 24,
+};
+
+one_slide = slides.addSlide("Piosenka o Duchu");
+one_slide.addText(text1, options);
+second_slide = slides.addSlide("Westchnienia miłosne");
+second_slide.addText(text2, options);
+
+slides.writeFile("12-04-2020");
 
 const client = redisServer.createClient({
-    host: "SG-slidesgenerator-32679.servers.mongodirector.com",
-    port: 6379,
-    password: "lTycnfuO7PFtx69k3ObyEsTfizlZy9JF"
-})
+  host: "SG-slidesgenerator-32679.servers.mongodirector.com",
+  port: 6379,
+  password: "lTycnfuO7PFtx69k3ObyEsTfizlZy9JF",
+});
 
 app.use(express.static("assets"));
 
-app.get('/songs', (req, res) => {
+app.get("/songs", (req, res) => {
   client.get("songs", (x, values) => {
     // res.writeHead(status_ok, {'Content-Type': mimetypes.html});
     res.write(JSON.stringify(values));
@@ -30,57 +62,51 @@ app.get('/songs', (req, res) => {
   });
 });
 
-
-app.get('/debug', (req, res) => {
+app.get("/debug", (req, res) => {
   const serve = (values) => {
     // [[key, value]]
     res.write(JSON.stringify(values));
     res.end();
-  }
+  };
 
   const getValues = (keys) => {
     const promises = [];
-    keys.forEach(key => {
+    keys.forEach((key) => {
       const promise = new Promise((resolve) => {
         client.get(key, (_, value) => {
           resolve([key, value]);
-        })
-      })
+        });
+      });
       promises.push(promise);
-    })
+    });
 
-    Promise.all(promises)
-      .then(serve);
-  }
+    Promise.all(promises).then(serve);
+  };
 
   client.keys("*", (_, keys) => {
-    getValues(keys)
+    getValues(keys);
   });
-
 });
-
 
 client.on("error", function (error) {
   console.error(error);
 });
 
 client.keys("*", function (err, res) {
-  console.log(res)
-})
+  console.log(res);
+});
 
+app.post("/songs", (req, res) => {
+  client.set;
+});
 
-app.post('/songs', (req, res) => {
-  client.set
-})
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "page.html"));
 });
 
 app.listen(3000, () => {
   console.log("Hey! Server is running");
 });
-
 
 // const mimetypes = {
 //   'html': 'text/html',
@@ -115,11 +141,9 @@ app.listen(3000, () => {
 
 // optional chaining
 
-
 // server.listen(port, hostname, () => {
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
-
 
 // app.use('/assets', express.static(path.join(__dirname, "assets")));
 
@@ -131,32 +155,29 @@ app.listen(3000, () => {
 //       });
 // });
 
-//może byś spróbował to 
+//może byś spróbował to
 //https://revealjs.com/#/fragments
 //do wyświetlania prezentacyj ??? :)
 //z www.education-ecosystem.com :0
 
+// let data = '';
+// let values = [];
+// client.keys("*", )
+// client.keys("*", async (x, values2) => {
+//   values = values2
+// })
+// .then(() => {
+//   return values.map(value => {
+//     return client.get(value, (x, value) => {
+//       data += value;
+//     })
+//   })
+// })
+// .then(Promise.all)
+// .then(() => {
+//   res.write(data);
+//   res.end();
+// });
+// console.log("test")
 
-  // let data = '';
-  // let values = [];
-  // client.keys("*", )
-  // client.keys("*", async (x, values2) => {
-  //   values = values2
-  // })
-  // .then(() => {
-  //   return values.map(value => {
-  //     return client.get(value, (x, value) => {
-  //       data += value;
-  //     })
-  //   })
-  // })
-  // .then(Promise.all)
-  // .then(() => {
-  //   res.write(data);
-  //   res.end();
-  // });
-  // console.log("test")
-
-
-
-  // Promise.all
+// Promise.all
